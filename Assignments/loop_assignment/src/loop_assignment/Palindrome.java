8d02f21b260ea20b2abc42a63a18b6fdce7051ff
Matt Moore <mtm5ef@virginia.edu>
2020-12-15 14:58:42 -0500

    Adding Palindrome to Loop Assignment

--- Assignments/loop_assignment/src/loop_assignment/Palindrome.java
+++ Assignments/loop_assignment/src/loop_assignment/Palindrome.java
@@ -4,7 +4,23 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		
-
+		String word = "racecar";
+		
+		if (isPalindrome(word) == true) {
+			System.out.println(word + " is a Palindrome");
+		}
+		else System.out.println(word + " is not a Palindrome");
+	}
+	
+	public static boolean isPalindrome(String word) {
+		int f=0, b=word.length()-1;
+		while (f < b) {
+			if (word.charAt(f) != word.charAt(b))
+					return false;
+			f++;
+			b--;
+		}
+		return true;
 	}
 
 }
