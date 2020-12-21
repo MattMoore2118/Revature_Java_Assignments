ed461512244a22ee841ea5099586950e013120f8
Matt Moore <mtm5ef@virginia.edu>
2020-12-21 10:22:33 -0500

    Finishing Inheritance

---
+++ Assignments/list_tasks/src/list_tasks/Queue.java
@@ -0,0 +1,17 @@
+package list_tasks;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class Queue {
+
+	public static void main(String[] args) {
+		
+		PriorityQueue q1=new PriorityQueue();
+		q1.add(123);
+		q1.add(15);
+		System.out.println(q1);
+
+	}
+
+}
